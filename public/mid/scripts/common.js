c2fba6414448548f731e3b0ca3b6165a08e49884
feaco <pheaco@gmail.com>
2016-08-05 09:38:56 +0800

    asd

--- public/mid/scripts/common.js
+++
@@ -1,4 +0,0 @@
-(function($,window,designWidth){
-	$('.F-box').css({'width':window.innerWidth+'px','height':window.innerHeight+'px','visibility':'visible'});
-	$('.F-page').css('zoom',window.innerWidth/designWidth);
-})(Zepto,window,640);
